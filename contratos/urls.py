ae5fec60a458e35d80e755807a20fda5d1c321ff
DiogotBatista <diogotadeub@gmail.com>
2025-04-14 11:07:50 -0300

    melhoria no navbar

--- contratos/urls.py
+++ contratos/urls.py
@@ -1,3 +1,4 @@
+#contratos/urls.py
 from django.urls import path
 from .views import (
     MenuView,
--- reunioes/urls.py
+++ reunioes/urls.py
@@ -1,3 +1,5 @@
+#reunioes/urls.py
+
 from django.urls import path
 from .views import (AtaReuniaoCreateView,
                     AtaReuniaoListView,
