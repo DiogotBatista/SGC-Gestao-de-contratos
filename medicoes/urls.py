aa74668a724d55ee017ca9a97aeaee399ebe21fc
DiogotBatista <diogotadeub@gmail.com>
2025-09-02 08:21:40 -0300

    correção API medições

--- medicoes/urls.py
+++ medicoes/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('<int:pk>/editar/', MedicaoUpdateView.as_view(), name='atualizar_medicao'),
     path('<int:pk>/excluir/', MedicaoDeleteView.as_view(), name='deletar_medicao'),
     path('api/powerbi/medicoes/', api_powerbi_medicoes, name='api_powerbi_medicoes'),
+    path('exportar-bi/', api_powerbi_medicoes, name='exportar_bi_alias'),
 ]
